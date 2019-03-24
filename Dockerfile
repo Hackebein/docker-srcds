b4252fd80c2bea534c3d43c2ba96b5466c43fcaa
Lars Olzem <hackebein@gmail.com>
2019-03-24 01:38:28 +0100

    Add support for dynamic GLST

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,11 @@ ARG BASEDIR=/opt/steam
 ONBUILD ARG BASEDIR=$BASEDIR
 ENV BASEDIR=$BASEDIR
 
+RUN apt update \
+ && apt install -y \
+		curl \
+        jq \
+ && apt clean
 COPY container/* $BASEDIR/
 
 EXPOSE 27015/tcp 27015/udp 27020/udp
--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -5,8 +5,13 @@ TVPORT=${TVPORT:-27020}
 CLIENTPORT=${CLIENTPORT:-27005}
 SPORT=${SPORT:-26900}
 SRCDSPARAMS=${SRCDSPARAMS:-}
+APIKEY=${APIKEY:-}
+GLST=${GLST:-}
+GLSTAPP=${GLSTAPP:-}
+GLSTMEMO=${GLSTMEMO:-$(hostname)}
 
 APPS=${APPS:-244310}
+IFS=',' read -ra APPS <<< "$APPS"
 for a in "${APPS[@]}" ; do
 	steamcmd \
 		+login anonymous \
@@ -15,11 +20,37 @@ for a in "${APPS[@]}" ; do
 		+quit
 done
 
+if [[ -z "${GLST}" && -n "${GLSTAPP}" && -n "${APIKEY}" ]]; then
+	IFS=- read STEAMID GLST <<<"$(curl \
+		-s \
+		-d "key=${APIKEY}&appid=${GLSTAPP}&memo=${GLSTMEMO}" \
+		'https://api.steampowered.com/IGameServersService/CreateAccount/v1/' \
+		| jq \
+			-e \
+			-r '.response | "\(.steamid)-\(.login_token)"' \
+	)"
+	if [[ "${STEAMID}" =~ ^[0-9]+$ && "${GLST}" =~ ^[0-9A-F]+$ ]]; then
+		echo "Created GLST: ${GLST} (${GLSTMEMO}) for APPID ${GLSTAPP}"
+	else
+		echo "GLST can't be created! Check your APIKEY, GLSTAPP and account requirements"
+		STEAMID=
+		GLST=
+	fi
+fi
+
 ./srcds_run \
 	-strictportbind \
 	-port "${PORT}" \
 	-tv_port "${TVPORT}" \
 	-clientport "${CLIENTPORT}" \
 	-sport "${SPORT}" \
+	+sv_setsteamaccount "${GLST}" \
 	"$(eval "echo ${SRCDSPARAMS}")" \
 	"${@}"
+
+if [ -n "${STEAMID}" ]; then
+	curl \
+		-s \
+		-d "key=${APIKEY}&steamid=${STEAMID}" \
+		'https://api.steampowered.com/IGameServersService/DeleteAccount/v1/'
+fi
